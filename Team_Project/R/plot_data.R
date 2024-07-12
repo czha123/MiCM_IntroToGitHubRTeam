68884ab969c5b221ac8198d26e3e0f76486688b0
sophia-chenn <sophia13chen@gmail.com>
2024-07-12 16:16:35 -0400

    Made a plot of canada's life expentancy

--- Team_Project/R/plot_data.R
+++ Team_Project/R/plot_data.R
@@ -1,1 +1,8 @@
-xx
+library(dplyr)
+library(ggplot2)
+
+file <- readRDS("./data/gapminder_unfiltered.rds")
+View(file)
+
+canada <- file %>% filter(country == "Canada")
+ggplot(aes(x = year, y = lifeExp), data = canada) + geom_point()
